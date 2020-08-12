2189bb6db92e5ebcae3ddeddefb9650223f67682
henry <hnr.leto11@gmail.com>
2020-08-12 14:18:29 -0400

    Added ability to guess whole word, colorize, and disallowment of repeat guesses.

--- Hangman.rb
+++ Hangman.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'yaml'
+require 'colorize'
 
 # the start of the board
 class Board
@@ -12,44 +13,51 @@ class Board
 end
 # the start of a game
 class Game
-  attr_reader :board, :guessed, :final, :answer
+  attr_reader :board, :guessed, :final, :answer, :user_guess, :player1, :swapped
   def initialize(player1)
     @player1 = player1
     @board = Board.new
-    @guessed = ['1']
+    @guessed = []
     @attempts = 1
     @mode = 0
-    @answer = answer
+    @final = board.word
+    @display_content = '_' * @final.length
     intro_text
   end
 
   def self.load_game
     save_file = File.open('save_file.yaml')
-    @game = YAML::load(save_file)
-    puts "\nWelcome back, #{@player1}"
+    @game = YAML.load(save_file)
+    puts "\nWelcome back, #{@player1}".cyan
     @game.game_play
   end
 
   def save_game
-    yaml = YAML::dump(self)
+    yaml = YAML.dump(self)
     File.open('save_file.yaml', 'w+') { |x| x.write yaml }
-    puts "Game saved\nPress ENTER to continue..."
   end
 
   def intro_text
     puts "\nIf you would like to start a new game, press 1\n\nIf you would like to load an old game, press 2"
     @mode = gets.chomp.to_i until @mode == 1 || @mode == 2
     if @mode == 1
+      puts "\nLets see if we can guess the word, shall we?"
+      save_text
       game_play
     else
       Game.load_game
     end
   end
 
-  def flipped_answer(guessed)
-    guessed_to_string = guessed.join
-    @answer = board.word
-    @final = answer.tr('^' + guessed_to_string, '_').chars.join
+  def flipped_answer(user_guess)
+    @final = board.word
+    @display_content.to_s
+
+    return unless user_guess.length == 1
+
+    @display_content.length.times do |index|
+      @display_content[index] = user_guess if @final[index].upcase == user_guess
+    end
   end
 
   def guesses
@@ -58,62 +66,64 @@ class Game
   end
 
   def game_play
-    flipped_answer(@guessed)
     guesses
     player_guess
   end
 
-  def error_check(user_guess)
-    return unless user_guess.length > 1
-
-    puts 'Please enter a single character guess'
-    @attempts -= 1 unless @attempts < 1
-  end
-
-  def letter_length(user_guess)
-    user_guess.each do |letter|
-      letter.length > 1 || letter.empty? ? @attempts -= 0 : @attempts += 1
-      puts(letter.length > 1 || letter.empty? ? "\n#{letter} -> invalid. 1 character guesses please." : '')
-    end
-  end
-
   def letters_guessed(user_guess)
+    swapped = @guessed.size.times.select { |i| @guessed[i] == user_guess } != []
+    user_guess = user_guess.split
     user_guess.each do |letter|
-      @guessed << letter unless letter.length > 1 || letter.empty?
+      @guessed << letter unless letter.include?('SAVE') || letter.include?('EXIT') || swapped == true
     end
     puts 'You have guessed:'
     p @guessed unless @guessed == []
   end
 
-  def win_con(guessed)
-    guessed_to_string = guessed.join
-    return unless @final.tr('^' + guessed_to_string, '_').chars.join == @answer.chars.join
+  def win_con
+    return unless @user_guess == @final
 
-    puts "Great job! You guessed #{@final} in #{@attempts} attempts"
+    puts "Nice Job! You guessed the word #{@final} in #{@attempts} attempts!".green
     exit
   end
 
-  def save_option
-    win_con(@guessed)
-    puts 'Would you like to save now? Y/N'
-    input = gets.chomp.upcase until input == 'Y' || input == 'N'
-    puts "\n"
-    puts(input == 'Y' ? save_game : 'Make a guess')
+  def save_option(user_guess)
+    win_con
+    if user_guess == 'EXIT'
+      puts 'Goodbye!'.cyan
+      exit
+    elsif user_guess == 'SAVE'
+      save_game
+      puts "\nGame saved...\nContinue guessing".cyan
+    else
+      win_con
+    end
+  end
+
+  def save_text
+    puts "\nAvailable options are: Save, Exit, or continue guessing."
+  end
+
+  def error_check(user_guess)
+    return unless @guessed.size.times.select { |i| @guessed[i] == user_guess } != []
+
+    puts 'You already guessed that! Guess again.'.red
+    @attempts -= 1 unless @attempts < 1
   end
 
   def player_guess
-    puts "\nLet's see if we can guess the word, shall we?"
     while @attempts < 12
-      p "This is attempt ##{@attempts}"
-      puts flipped_answer(@guessed)
-      save_option
-      @user_guess = gets.chomp.upcase.split
+      puts "\nThis is attempt ##{@attempts}".yellow
+      puts "\n#{@display_content.chars.join(' ')}"
+      @user_guess = gets.chomp.upcase
       error_check(@user_guess)
-      letter_length(@user_guess)
       letters_guessed(@user_guess)
-      win_con(@guessed)
+      puts flipped_answer(@user_guess)
+      save_option(@user_guess)
+      win_con
+      @attempts += 1
     end
-    puts "\nSorry #{@player1}, the answer was #{@answer}."
+    puts "\nSorry #{@player1}, the answer was #{@final}.".yellow
   end
 end
 
